df4d41aaf50b95d63301d6f82f88b63cb73a5909
Teodor Yanchev <teo.yanchev93@gmail.com>
2021-10-06 00:50:13 +0300

    1-9 update

--- 07-FunctionsProceduresTriggersAndTransactions/1-9.sql
+++ 07-FunctionsProceduresTriggersAndTransactions/1-9.sql
@@ -16,3 +16,30 @@ AS
 EXEC usp_GetEmployeesSalaryAboveNumber 48100
 
 GO
+
+--03. Town Names Starting With
+CREATE PROC usp_GetTownsStartingWith (@town VARCHAR(100))
+AS
+SELECT [Name] FROM Towns
+	WHERE [Name] LIKE @town + '%'
+
+EXEC usp_GetTownsStartingWith 'b'
+
+--04. Employees from Town
+GO
+CREATE PROC usp_GetEmployeesFromTown (@townName VARCHAR(100))
+AS
+SELECT FirstName, LastName FROM Employees AS e
+	JOIN Addresses AS a ON a.AddressID = e.AddressID
+	JOIN Towns AS t  ON t.TownID = a.TownID
+	WHERE t.[Name] = @townName
+
+EXEC usp_GetEmployeesFromTown 'Sofia'
+
+--05. Salary Level Function
+GO
+CREATE FUNCTION ufn_GetSalaryLevel(@salary DECIMAL(18,4))
+RETURNS @result
+(
+	DECLARE @result VARCHAR(10)
+)
